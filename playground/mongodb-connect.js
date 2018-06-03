1c5075cfe3e9661f7eb3fee07ecef609e724f2d6
Ayush Ravi <ayushravirai@gmail.com>
2018-06-03 12:18:52 +0530

    something addedd

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -10,9 +10,10 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
 
     // Insert Some Data in the server
     const db = client.db('TodoApp');
-    // db.collection('Todos').insertOne({
-    //     text : "Kuch karna hain bhai mujhe kuch to karo",
-    //     completed : false
+    // db.collection('Users').insertOne({
+    //     name : "Ayush Ravi",
+    //     age : 25,
+    //     hobbies : "like to enjoy :P"
     // }, (error, result) => {
     //     if (error){
     //         return console.log("Unable to insert this shit", error);
@@ -20,18 +21,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
     //     console.log(result.ops);
     // });
 
-    // Find all data 
-    var data = db
-        .collection('Todos')
-        .find({
-            completed : true
-        }).toArray()
-        .then((documents) => {
-            console.log(JSON.stringify(documents, undefined, 2));
-        }).catch((error) => {
-            console.log("Unable to fetch todos ", error)
-        });
+    // // Find all data 
+    // var data = db
+    //     .collection('Todos')
+    //     .find({
+    //         completed : true
+    //     }).toArray()
+    //     .then((documents) => {
+    //         console.log(JSON.stringify(documents, undefined, 2));
+    //     }).catch((error) => {
+    //         console.log("Unable to fetch todos ", error)
+    //     }); 
 
+
+    db.collection('Users').findOneAndUpdate({
+        _id : new ObjectID('5b138d3d56b2b126efa37fd2')
+    },{
+        $set : {
+            name : "Aakshi"
+        },
+        $inc : {
+            age : -5
+        }
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log("Some shitty error occured : ", error);
+    });
     // console.log(data);
 
     client.close();
